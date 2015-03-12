9d705bccee6d2a41cd6cf8c1949ecbcb5716f453
Imen Chebbi <chabi.imen@gmail.com>
2015-03-12 16:25:59 +0100

    Try new authentication method.

--- server/auth/local/index.js
+++ server/auth/local/index.js
@@ -5,14 +5,27 @@ var passport = require('passport');
 var auth = require('../auth.service');
 var router = express.Router();
 
-// route to log in
-router.post('/login', passport.authenticate('local'), function (req, res) {
-    res.send(req.user);
-});
-
 router.get('/loggedin', function (req, res) {
     res.send(req.isAuthenticated() ? req.user : '0');
 });
+
+// route to log in
+router.post('/login', passport.authenticate('local'), function (req, res) {
+ res.send(req.user);
+ });
+
+/*router.post('/login', function (req, res, next) {
+    var authenticate = passport.authenticate('local', function (err, user) {
+        if (err) return next(err);
+        req.logIn(user, function(err) {
+            if (err) { return next(err); }
+            return res.send(user);
+        });
+
+    });
+
+    authenticate(req, res, next);
+});*/
 
 // route to log out
 router.post('/logout', function (req, res) {
