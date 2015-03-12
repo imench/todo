2842aef5532cb12e6756b14913c2d9d6196e3862
Imen Chebbi <chabi.imen@gmail.com>
2015-03-12 16:21:11 +0100

    modify port and root configuration

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,6 +1,9 @@
+var path = require('path');
 module.exports = {
-    DOMAIN: 'http://localhost:8080',
+    DOMAIN: 'http://localhost:9000',
     db: 'mongodb://localhost/mean',
-    sessionSecret: 'developmentSessionSecret'
+    sessionSecret: 'developmentSessionSecret',
+    port: process.env.PORT || 9000,
+    root: path.normalize(__dirname + '/../../..')
 };
 
--- server/config/express.js
+++ server/config/express.js
@@ -9,13 +9,25 @@ var bodyParser = require('body-parser');
 //var methodOverride = require('method-override');
 var session = require('express-session');
 var passport = require('passport');
+var path = require('path');
 
 module.exports = function (app) {
+    var env = app.get('env');
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     app.use(express.static(__dirname + '/../../client'));
-
     // app.use(methodOverride());
+
+    if ('production' === env) {
+        app.use(express.static(path.join(config.root, 'public')));
+        app.set('appPath', config.root + '/public');
+    }
+
+    if ('development' === env || 'test' === env) {
+        app.use(express.static(path.join(config.root, '.tmp')));
+        app.use(express.static(path.join(config.root, 'client')));
+        app.set('appPath', 'client');
+    }
 
     app.use(session({
         saveUninitialized: true,
